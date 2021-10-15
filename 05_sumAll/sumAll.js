c72c1bb2d29e9c9ac489bda065d5cb0b7f40fc22
Estagiario <estagiario@florence.edu.br>
2021-10-15 10:51:20 -0300

    Exercicio 5 com problema no teste onde o numero e menor que 0

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let array = Array.from(args);
+    let sum = 0;
+    arraySum = [];
+    // sum all values between two numbers parameters !
 
-};
+    for (let i = 0; i < array.length; i ++) {
+        let elem = array[i]
+        if (typeof(elem) == 'number') {
+            arraySum.push(elem)
+        } else {
+            return 'ERROR'
+        }
+    }
+    arraySum.sort()
 
+    for (let i = arraySum[0]; i <= arraySum[1]; i ++){
+        sum += i;
+    }
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
