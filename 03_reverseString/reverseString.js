79efd041c5e69265d0204b28485719b029847843
Leonardo Garreto <leonardogarreto@gmail.com>
2021-10-17 16:14:18 -0300

    changes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function(str) {
+
+const reverseString = function(str) { 
+let arrayStr = str.split('')
+let reversedArray = [];
+for (let i = 0; i < arrayStr.length; i++){
+    let letter = arrayStr[(arrayStr.length -1) - i]
+    reversedArray.push(letter);
+    }
+    return reversedArray.join('')
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let elements = Array.from(args);
+    
+    for (let i = 0; i < elements.length; i ++){
+        let positionElement = array.indexOf(elements[i]);
+        let positionFinalElement = array.indexOf(elements[i]) + 1;
+        array.splice(positionElement, positionFinalElement)
+    }
+    return array;
+    
 };
 
 // Do not edit below this line
