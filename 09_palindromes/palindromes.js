813ad9836a8938ffcb302f39448aa9b3e7b9a05e
Leonardo Garreto <leonardogarreto@gmail.com>
2021-10-18 19:13:41 -0300

    Test 2 of 09 palindromes error

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arrayString = string.toLowerCase().split("")  
+    for (let i = 0; i < arrayString.length; i++) {
+        if (arrayString[i] == "!") {
+            let index = arrayString.indexOf(arrayString[i])
+            arrayString.splice(index, 1)
+        }
+    }
+
+    let reverseArray = arrayString.reverse() // reverse array
+    let reverseStr = reverseArray.join("") // array to string
+    return (reverseStr == string);        
 
 };
 
