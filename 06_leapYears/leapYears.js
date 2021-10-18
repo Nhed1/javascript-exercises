356898acd4fd3d3c2a10fa7f3d62088dfaa2fd08
Estagiario <estagiario@florence.edu.br>
2021-10-18 09:20:29 -0300

    exercise 06 done, just see first about the number be divisible by 100 and 400 and then you try with 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap years are divisible by four, but years divisible 
+    // by 100 are not leap years unless they are divisible by 400
+   
+    if (year % 100 == 0){
+        if (year % 400 == 0){
+            return true
+        } else {
+            return false
+        }
+    } else if (year % 4 == 0){
+        return true;
+    } else {
+        return false;
+    }
 
+    
 };
 
 // Do not edit below this line
