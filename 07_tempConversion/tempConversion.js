dce20b241d54abd46eb97b6519c2e98d916d8f92
Estagiario <estagiario@florence.edu.br>
2021-10-18 10:41:18 -0300

    exercise 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let celc = (far - 32) * 5/9
+  celc = Math.round(celc * 10) / 10
+  return celc
 };
 
-const ctof = function() {
-
+const ctof = function(celc) {
+  let far = celc * 9/5 + 32 
+  far = Math.round(far * 10) / 10
+  return far
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
