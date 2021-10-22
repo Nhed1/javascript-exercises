86b13c1321577e999fe68ad192f93d8cb11a6db7
Leonardo Garreto <leonardogarreto@gmail.com>
2021-10-22 10:06:04 -0300

    exercise 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,18 @@
 const palindromes = function (string) {
     let arrayString = string.toLowerCase().split("")  
-    for (let i = 0; i < arrayString.length; i++) {
-        if (arrayString[i] == "!") {
-            let index = arrayString.indexOf(arrayString[i])
-            arrayString.splice(index, 1)
-        }
-    }
 
+    for (let i = 0; i < arrayString.length; i++) {
+        let index = arrayString.indexOf(arrayString[i])
+        if (arrayString[i] == "!" || arrayString[i] == "." || arrayString[i] == ","){
+            arrayString.splice(index, 1)
+        } if (arrayString[i] == " ") // remove empty space after removing ", . !"
+          arrayString.splice(index, 1)
+    }
+    string = arrayString.join("") // join the text without "!,."
+    
     let reverseArray = arrayString.reverse() // reverse array
     let reverseStr = reverseArray.join("") // array to string
-    return (reverseStr == string);        
-
+    return (reverseStr == string); 
 };
 
 // Do not edit below this line
