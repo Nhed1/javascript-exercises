5c94b86689b8c08892c866747d47d288df7f17cf
Leonardo Garreto <leonardogarreto@gmail.com>
2021-10-23 14:24:37 -0300

    exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age,
+        oldest,
+        maxAge = 0,
+        currentYear = new Date().getFullYear()
 
+    for (let i = 0; i < people.length; i++){
+        let person = people[i],
+            birth = person.yearOfBirth,
+            death = person.yearOfDeath
+
+        age = death - birth
+        if (isNaN(age)){ // check if is Not a number
+            age = currentYear - birth
+        }
+        if (age > maxAge){
+            oldest = person
+            maxAge = age
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
