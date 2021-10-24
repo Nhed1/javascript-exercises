c6dcb726b32b4f38b2e986de5218b04a90629a51
Leonardo Garreto <leonardogarreto@gmail.com>
2021-10-24 18:54:16 -0300

    I could not  do this exercise

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,27 @@
-const caesar = function() {
+const caesar = function(word,shift) {
+  let ap = 'abcdefghijklmnopqrstuvwxyz',
+  alphabet = ap.split(""),
+  wordArray = word.split("")
+
+  for (let i = 0; i < wordArray.length; i++){    
+    let char = word[i],
+    isALetter = alphabet.includes(char),
+    accumulator = ''
+
+    if (!isALetter) {
+      accumulator += char;
+    } else {
+      let charIndex = alphabet.indexOf(char)
+
+      accumulator += alphabet[charIndex + shift] ||
+       alphabet[charIndex - shift]
+    }
+  }
+  return accumulator
 
 };
+
+console.log(caesar('Mjqqt, Btwqi!', -5))
 
 // Do not edit below this line
 module.exports = caesar;
